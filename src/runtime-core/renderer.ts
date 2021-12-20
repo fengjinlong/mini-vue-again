e4c82cc8cc4eb64995ac14bbf330f5d2159cf933
fjl <18618166564@163.com>
2021-12-20 15:06:26 +0800

    diff

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -220,6 +220,76 @@ export function createRenderer(options) {
         hostRemove(c1[i].el);
         i++;
       }
+    } else {
+      // 中间对比,经过以上逻辑已经找到了两个临界点
+      /**
+       * 第五种情况-1。删除d，修改c
+       * 旧 ab cd fg
+       * 新 ab ec fg
+       * 1 旧的里面存在，新的不存在（d），那么需要删除 d。
+       * 如果在ec里面遍历看是否存在d，那么时间复杂度是O(n),如果用 key 映射，那么时间复杂度是O(1)
+       *
+       */
+
+      let s1 = i;
+      let s2 = i;
+
+      /**
+       * 优化点：当新节点的个数小于老节点点个数，也就是新的已经patch完毕，但是老节点还存在，那么老节点剩下的无需在对比，直接删除
+       * 老 ab cedm fg，新 ab ec fg,当新节点的ec对比完毕，老节点还剩dm，那么直接删除，无需对比
+       * 
+       * toBePatched 新节点需要patch的个数
+       * patched 已经处理的个数
+       *  
+      */
+      const toBePatched = e2 -s2 +1
+      let patched = 0
+
+
+      // 映射关系
+      const keyToNewIndexMap = new Map();
+
+      // 新的映射关系
+      for (let i = s2; i <= e2; i++) {
+        const nextChild = c2[i];
+        keyToNewIndexMap.set(nextChild.key, i);
+      }
+
+      // 老的映射关系
+      for (let i = s1; i <= e1; i++) {
+        // 老节点 prevChild
+        const prevChild = c1[i];
+        if (patched >= toBePatched) {
+          // 新的已经对比完，但是老的还没完事。直接删除
+          hostRemove(prevChild.el)
+          // 进入下一次循环
+          continue;
+        }
+        let newIndex;
+        /**
+         *  如果 newIndex 存在，说明 prevChild 在新的里面存在。
+         *  如果用户写了key，用key映射查找。如果没写key,用循环查找
+         */
+        if (newIndex !== null) {
+          newIndex = keyToNewIndexMap.get(prevChild.key);
+        } else {
+          for (let j = s2; j <= e2; j++) {
+            if (isSameNodeType(c2[j], prevChild)) {
+              newIndex = j;
+              break;
+            }
+          }
+        }
+
+        if (newIndex === undefined) {
+          // 说明不存在prevChild，删掉老的 prevChild
+          hostRemove(prevChild.el)
+        } else {
+          // 存在，继续进行深度对比
+          patch(prevChild, c2[newIndex], container, parentComponent, null)
+          patched++
+        }
+      }
     }
   }
 
